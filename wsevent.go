0f6c30e4a074932616e2b53b8285548a6cdfc4b4
Vibhav Pant <vibhavp@gmail.com>
2015-10-20 14:45:20 +0530

    Use RWLocks

--- wsevent.go
+++ wsevent.go
@@ -17,18 +17,17 @@ type Client struct {
 	//Session ID
 	ID string
 	//Rooms the client has been added to
-	rooms []string
+	rooms     []string
+	roomsLock *sync.RWMutex
 
-	conn      *ws.Conn
-	readLock  *sync.Mutex
-	writeLock *sync.Mutex
-	roomsLock *sync.Mutex
+	conn     *ws.Conn
+	connLock *sync.RWMutex
 }
 
 //Server
 type Server struct {
 	rooms     map[string]([]*Client)
-	roomsLock *sync.Mutex
+	roomsLock *sync.RWMutex
 
 	//The extractor function reads the byte array and the message type
 	//and returns the event represented by the message.
@@ -37,8 +36,8 @@ type Server struct {
 	//the disconnected client's session ID
 	OnDisconnect func(string)
 
-	calls   map[string]func([]byte, int) ([]byte, int)
-	mapLock *sync.Mutex
+	handlers     map[string]func([]byte, int) ([]byte, int)
+	handlersLock *sync.RWMutex
 
 	newClient chan *Client
 }
@@ -59,9 +58,8 @@ func (s *Server) NewClient(upgrader ws.Upgrader, w http.ResponseWriter, r *http.
 		ID:        genId(r),
 		rooms:     arr,
 		conn:      conn,
-		readLock:  new(sync.Mutex),
-		writeLock: new(sync.Mutex),
-		roomsLock: new(sync.Mutex)}
+		connLock:  new(sync.RWMutex),
+		roomsLock: new(sync.RWMutex)}
 	s.newClient <- client
 
 	return client, nil
@@ -69,8 +67,8 @@ func (s *Server) NewClient(upgrader ws.Upgrader, w http.ResponseWriter, r *http.
 
 //A thread-safe variant of WriteMessage
 func (c *Client) Emit(data []byte, messageType int) error {
-	c.writeLock.Lock()
-	defer c.writeLock.Unlock()
+	c.connLock.Lock()
+	defer c.connLock.Unlock()
 
 	return c.conn.WriteMessage(messageType, data)
 }
@@ -78,11 +76,11 @@ func (c *Client) Emit(data []byte, messageType int) error {
 //Return a new server object
 func NewServer() *Server {
 	s := &Server{
-		rooms:     make(map[string]([]*Client)),
-		roomsLock: new(sync.Mutex),
-		calls:     make(map[string](func([]byte, int) ([]byte, int))),
-		mapLock:   new(sync.Mutex),
-		newClient: make(chan *Client),
+		rooms:        make(map[string]([]*Client)),
+		roomsLock:    new(sync.RWMutex),
+		handlers:     make(map[string](func([]byte, int) ([]byte, int))),
+		handlersLock: new(sync.RWMutex),
+		newClient:    make(chan *Client),
 	}
 
 	return s
@@ -116,8 +114,8 @@ func (s *Server) Broadcast(room string, data []byte, messageType int) {
 
 func (c *Client) cleanup(s *Server) {
 	c.conn.Close()
-	c.roomsLock.Lock()
-	defer c.roomsLock.Unlock()
+	c.roomsLock.RLock()
+	defer c.roomsLock.RUnlock()
 
 	for _, room := range c.rooms {
 		s.roomsLock.Lock()
@@ -143,9 +141,9 @@ func (s *Server) Listener() {
 
 				callName := s.Extractor(data, messageType)
 
-				s.mapLock.Lock()
-				f, ok := s.calls[callName]
-				s.mapLock.Unlock()
+				s.handlersLock.RLock()
+				f, ok := s.handlers[callName]
+				s.handlersLock.RUnlock()
 
 				if !ok {
 					continue
@@ -159,7 +157,7 @@ func (s *Server) Listener() {
 //Registers a callback for the event string. The callback must take two arguments,
 //a byte array it's type, and return a byte array and it's type.
 func (s *Server) On(event string, f func([]byte, int) ([]byte, int)) {
-	s.mapLock.Lock()
-	s.calls[event] = f
-	s.mapLock.Unlock()
+	s.handlersLock.Lock()
+	s.handlers[event] = f
+	s.handlersLock.Unlock()
 }
